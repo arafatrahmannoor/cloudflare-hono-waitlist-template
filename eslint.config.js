5f1e0ffc545985ede8774de1859be308c9227c58
Arafat Rahman Noor <rahman15-6011@diu.edu.bd>
2026-09-03 09:39:47 +0600

    feat: add React support and new components
    - Updated package.json to include React, TypeScript, ESLint, and Prettier dependencies.
    - Added a new React component 'Hi' to demonstrate integration with Astro.
    - Modified index.astro to include the new 'Hi' component.
    - Configured TypeScript to support React JSX syntax.
    - Created ESLint configuration for Astro and React.
    - Added VSCode settings and launch configurations for improved development experience.

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    
+    files: ["*.astro", "*.ts", "*.tsx"],
+    processor: "astro/client-side-ts",
+    rules: {
+
+    },
+  },
+];
