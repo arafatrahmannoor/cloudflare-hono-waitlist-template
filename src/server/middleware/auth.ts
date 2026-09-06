cec8b3a6c249419d4603901dece56313d041e631
Arafat Rahman Noor <rahman15-6011@diu.edu.bd>
2026-09-07 01:39:30 +0600

    feat(auth): add access authentication middleware using JWT verification

---
+++ src/server/middleware/auth.ts
@@ -0,0 +1,30 @@
+import { createMiddleware } from "hono/factory";
+import { createRemoteJWKSet, jwtVerify } from "jose";
+
+export const accessAuth = createMiddleware(async (c, next) => {
+  // const ACCESS_AUD =
+  //   "1dc88100dc5f540fee23727b1555ab8c3607c3c21d51ea93db1a592f1c95e9df";
+  // const ACCESS_JWKS = createRemoteJWKSet(
+  //   new URL(
+  //     "https://green-fire-15ed.cloudflareaccess.com/cdn-cgi/access/certs",
+  //   ),
+  // );
+
+  if (!c.env.POLICY_ID) {
+    return c.json("Missing Access identity", 403);
+  }
+  const token = c.req.header("CF-Access-Jwt-Assertion");
+
+  if (!token) {
+    return c.json("Missing Access identity", 403);
+  }
+
+  const ACCESS_JWKS = createRemoteJWKSet(
+    new URL(`${c.env.CF_ACCESS_DOMAIN}/cdn-cgi/access/certs`),
+  );
+
+  await jwtVerify(token, ACCESS_JWKS, {
+    audience: c.env.POLICY_ID,
+  });
+  await next();
+});
